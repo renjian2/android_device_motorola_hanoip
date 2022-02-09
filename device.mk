70250372fd90e4f013d1749614ff130f6d8fb415
Nikhil <renjian503@gmail.com>
2022-02-09 15:11:32 +0530

    change path

--- device.mk
+++ device.mk
@@ -105,4 +105,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 # Apex libraries
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+    $(OUT_DIR)/target/product/hanoip/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
